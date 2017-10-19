92e50b65af1fd8aaec00c6a58c87616699fa645c
MaxDiaz2997 <maxdiaz194@gmail.com>
2017-10-19 16:57:48 -0500

    este cambio se hizo jugando

---
+++ index.js
@@ -0,0 +1,19 @@
+//console.log("Hola node")
+const lodash = require("lodash")
+const obj={
+  nombre:"max",
+  edad:"19",
+  email:"maxdiaz194@gmail.com",
+  tw:"@max"
+}
+console.log(obj.email)
+
+
+//console.log("hola mundo");
+//function hola() {
+//return "hola mundo x2"
+//}
+//console.log(hola());
+//console.log(2+2)
+//var lista = [1, 2, 3, 4, 5]
+//console.log(lista)
